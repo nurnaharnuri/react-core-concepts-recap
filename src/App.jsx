6f9be677fad5a6cc522e004b884e34eb603fa32a
nurnaharnuri <nurnaharnuri234@gmail.com>
2023-09-11 12:45:18 +0600

    components added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Posts from "./posts";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <ol>
+        <li>Components</li>
+      </ol>
+      <hr />
+      <Posts></Posts>
+    </>
+  );
+}
+
+export default App;
